989e2caacbff75439bfb64e92aa877f4650fd196
GitHub Action <action@github.com>
2020-08-05 09:55:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangzhen99324/myfundtest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wangzhen99324/myfundtest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangzhen99324.myfundtest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangzhen99324.myfundtest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wangzhen99324/myfundtest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangzhen99324/myfundtest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangzhen99324.myfundtest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangzhen99324.myfundtest.MyBundle
 
 class MyProjectService(project: Project) {
 
